36b5aedd907dfc3ed19c80df45505a5faed8e33b
ufrarufa <for_rove@naver.com>
2021-04-27 16:32:43 +0900

    Cunsomer 로 Action 실행하ㅣ

--- ch15/context-tt/src/App.js
+++ ch15/context-tt/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import ColorBox from "./components/ColorBox";
 import ColorContext from "./context/color";
 import ColorProvider from "./context/ColorContext";
+import SelectColors from "./components/SelectColors";
 
 function App() {
   return (
     <ColorProvider>
       {/* <ColorContext.Provider value={{ color: "red" }}> */}
       <div className="App">
+        <SelectColors></SelectColors>
         <ColorBox></ColorBox>
       </div>
       {/* </ColorContext.Provider> */}
